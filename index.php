bfc0e61ee531ebceee3a131e32fc4c101a595d05
Sartori <thiagosartori@live.com>
2016-08-20 17:10:47 -0300

    second commit

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-Hello world!
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo="Hello world!";
+    ?>
+  </body>
+</html>
