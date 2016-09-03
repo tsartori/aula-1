6a01f92c015745d66bb3f331ea832ee2ab0c2341
tsartori <thiagosartori@live.com>
2016-09-03 17:46:50 -0300

    2016-09-03 17:46

--- exemplo3/layout/body.php
+++ exemplo3/layout/body.php
@@ -1,12 +1,20 @@
 <body>
   <?php
- $variavel = 0;
+  $variavel1 = 'teste';
+  $variavel2 = "Frase {$variavel1}!";
+  $variavel3 = array('Maçã', 'Banana', 'Pêra', 'Xuxu', 'Quiabo');
+  $variavel4 = array('a'=> 'Maçã', 'b'=> 'Banana', 'c'=> 'Pêra', 'd'=> 'Xuxu', 'e'=> 'Quiabo');//aray associativo - semelhante hashmap do java
 
- echo $variavel;
-
+ echo $variavel1;
  echo "<br>";
- echo $variavel;
+ echo $variavel2;
+ echo "<br>";
+ echo implode(',',$variavel3);
+ echo "<br>";
+ echo $variavel3[0];
+ echo "<br>";
+ echo $variavel4['b'];
 
 
-   ?>
+ ?>
 </body>
