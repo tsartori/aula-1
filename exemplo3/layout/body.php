a4375502c44df966bb09e6b4e7b690410c84ff53
tsartori <thiagosartori@live.com>
2016-09-03 18:04:44 -0300

    Fim aula 2016-09-03

--- exemplo3/layout/body.php
+++ exemplo3/layout/body.php
@@ -4,17 +4,21 @@
   $variavel2 = "Frase {$variavel1}!";
   $variavel3 = array('Maçã', 'Banana', 'Pêra', 'Xuxu', 'Quiabo');
   $variavel4 = array('a'=> 'Maçã', 'b'=> 'Banana', 'c'=> 'Pêra', 'd'=> 'Xuxu', 'e'=> 'Quiabo');//aray associativo - semelhante hashmap do java
+  $variavel5 = false;
 
- echo $variavel1;
- echo "<br>";
- echo $variavel2;
- echo "<br>";
- echo implode(',',$variavel3);
- echo "<br>";
- echo $variavel3[0];
- echo "<br>";
- echo $variavel4['b'];
-
+  echo $variavel1;
+  echo "<br>";
+  echo $variavel2;
+  echo "<br>";
+  echo implode(',',$variavel3);
+  echo "<br>";
+  echo $variavel3[0];
+  echo "<br>";
+  echo $variavel4['b'];
+  echo "<br>";
+  echo $variavel5;
+  echo "<br>";
+  require 'printer.php'
 
  ?>
 </body>
---
+++ exemplo3/layout/printer.php
@@ -0,0 +1,20 @@
+<?php
+   $vArray = array();
+
+   $vArray[0]=variavel1;
+   $vArray[1]=variavel2;
+   $vArray[2]=variavel3;
+   $vArray[3]=variavel4;
+   $vArray[4]=variavel5;
+
+   echo "<pre>";
+   var_dump($vArray);
+   echo "</pre>";
+/*
+   foreach ($vArray as $indice => $valor) {
+     echo "<pre>";
+     var_dump($vArray[$indice, $valor]);
+     echo "</pre>";
+   }
+   */
+   
